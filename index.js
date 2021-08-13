0db9891d4c3b48d055ccf4bea6e6428364dc5193
khushbu <khushbum.wa@gmail.com>
2021-08-13 11:41:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 export {intersectionArea, circleCircleIntersection, circleOverlap, circleArea,
         distance} from "./src/circleintersection";
-export {venn, greedyLayout, scaleSolution, normalizeSolution,bestInitialLayout,
+export {venn, greedyLayout, scaleSolution, normalizeSolution,
         lossFunction, disjointCluster, distanceFromIntersectArea} from "./src/layout";
 export {VennDiagram, wrapText, computeTextCentres, computeTextCentre,sortAreas,
         circlePath, circleFromPath, intersectionAreaPath} from "./src/diagram";
-
